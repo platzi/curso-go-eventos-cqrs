96e2d8c4548a99e19df83166848ee8101e89c2eb
Nestor Escoto <nestor@smartifice.com>
2022-02-13 00:25:35 -0600

    Implementando Indexacion mediante Elastic Search

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module platzi.com/go/cqrs
 go 1.17
 
 require (
+	github.com/elastic/go-elasticsearch/v7 v7.17.0 // indirect
 	github.com/lib/pq v1.10.4 // indirect
 	github.com/nats-io/nats.go v1.13.0 // indirect
 	github.com/nats-io/nkeys v0.3.0 // indirect
---
+++ search/elastic.go
@@ -0,0 +1,99 @@
+package search
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+
+	elastic "github.com/elastic/go-elasticsearch/v7"
+	"platzi.com/go/cqrs/models"
+)
+
+type ElasticSearchRepository struct {
+	client *elastic.Client
+}
+
+func NewElastic(url string) (*ElasticSearchRepository, error) {
+	client, err := elastic.NewClient(elastic.Config{
+		Addresses: []string{url},
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &ElasticSearchRepository{
+		client: client,
+	}, nil
+}
+
+func (r *ElasticSearchRepository) Close() {
+	//
+}
+
+func (r *ElasticSearchRepository) IndexFeed(ctx context.Context, feed models.Feed) error {
+	body, _ := json.Marshal(feed)
+	_, err := r.client.Index(
+		"feeds",
+		bytes.NewReader(body),
+		r.client.Index.WithDocumentID(feed.ID),
+		r.client.Index.WithContext(ctx),
+		r.client.Index.WithRefresh("wait_for"),
+	)
+	return err
+}
+
+func (r *ElasticSearchRepository) SearchFeed(ctx context.Context, query string) (results []models.Feed, err error) {
+	var buf bytes.Buffer
+	searchQuery := map[string]interface{}{
+		"query": map[string]interface{}{
+			"multi_match": map[string]interface{}{
+				"query":            query,
+				"fields":           []string{"title", "description"},
+				"fuzzines":         3,
+				"cutoff_frequency": 0.0001,
+			},
+		},
+	}
+	if err = json.NewEncoder(&buf).Encode(searchQuery); err != nil {
+		return nil, err
+	}
+
+	res, err := r.client.Search(
+		r.client.Search.WithContext(ctx),
+		r.client.Search.WithIndex("feeds"),
+		r.client.Search.WithBody(&buf),
+		r.client.Search.WithTrackTotalHits(true),
+	)
+	defer res.Body.Close()
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		if err := res.Body.Close(); err != nil {
+			results = nil
+		}
+	}()
+
+	if res.IsError() {
+		return nil, errors.New("elasticsearch error " + res.String())
+	}
+
+	var eRes map[string]interface{}
+	if err := json.NewDecoder(res.Body).Decode(&r); err != nil {
+		return nil, err
+	}
+	var feeds []models.Feed
+	for _, hit := range eRes["hits"].(map[string]interface{})["hits"].([]interface{}) {
+		feed := models.Feed{}
+		source := hit.(map[string]interface{})["_source"]
+		marshal, err := json.Marshal(source)
+		if err != nil {
+			return nil, err
+		}
+		if err := json.Unmarshal(marshal, &feed); err == nil {
+			feeds = append(feeds, feed)
+		}
+	}
+	return feeds, nil
+}
