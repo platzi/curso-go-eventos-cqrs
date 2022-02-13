0e5b97bd387576d9e6389ecc52eb273f767e538d
Nestor Escoto <nestor@smartifice.com>
2022-02-13 00:38:53 -0600

    Creando Feed Service

---
+++ feed-service/handler.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/segmentio/ksuid"
+	"platzi.com/go/cqrs/events"
+	"platzi.com/go/cqrs/models"
+	"platzi.com/go/cqrs/repository"
+)
+
+type createFeedRequest struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+}
+
+func createFeedHandler(w http.ResponseWriter, r *http.Request) {
+	var req createFeedRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	createdAt := time.Now().UTC()
+	id, err := ksuid.NewRandom()
+	if err != nil {
+		http.Error(w, "failed to create user", http.StatusInternalServerError)
+		return
+	}
+	feed := models.Feed{
+		ID:          id.String(),
+		Title:       req.Title,
+		Description: req.Description,
+		CreatedAt:   createdAt,
+	}
+
+	if err := repository.InsertFeed(r.Context(), &feed); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	if err := events.PublishCreatedFeed(r.Context(), &feed); err != nil {
+		log.Printf("failed to publish created feed event: %v", err)
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(feed)
+}
--- go.mod
+++ go.mod
@@ -4,9 +4,12 @@ go 1.17
 
 require (
 	github.com/elastic/go-elasticsearch/v7 v7.17.0 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/kelseyhightower/envconfig v1.4.0 // indirect
 	github.com/lib/pq v1.10.4 // indirect
 	github.com/nats-io/nats.go v1.13.0 // indirect
 	github.com/nats-io/nkeys v0.3.0 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
+	github.com/segmentio/ksuid v1.0.4 // indirect
 	golang.org/x/crypto v0.0.0-20220210151621-f4118a5b28e2 // indirect
 )
